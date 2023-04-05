85ab0357dcf66117fc210b6010b2a8db28d10d02
HughMorris01 <gfarrell2@Live.com>
2023-04-05 11:48:55 -0400

    Initial Commit, Create Base Components

---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const pageLinks = [
+  { id: 1, href: '#home', text: 'home' },
+  { id: 2, href: '#about', text: 'about' },
+  { id: 3, href: '#services', text: 'services' },
+  { id: 4, href: '#footer', text: 'footer' },
+];
+
+export const socialLinks = [
+  { id: 1, href: 'http://www.twitter.com', className: 'fab fa-twitter' },
+  { id: 2, href: 'http://www.facebook.com', className: 'fab fa-facebook' },
+  {
+    id: 3,
+    href: 'http://www.squarespace.com',
+    className: 'fab fa-squarespace',
+  },
+];
