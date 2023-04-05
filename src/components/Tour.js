8872f433b95f5db01888a0ef45e47d538df6d22a
HughMorris01 <gfarrell2@Live.com>
2023-04-05 17:12:32 -0400

    Test 3.....

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -4,7 +4,7 @@ function Tour({ imgSrc, date, name, country, length, price }) {
   return (
     <article className="tour-card">
       <div className="tour-img-container">
-        <img src={imgSrc} className="tour-img" alt="tour image" />
+        <img src={imgSrc} className="tour-img" alt="tour" />
         <p className="tour-date">{date}</p>
       </div>
       <TourInfo name={name} country={country} length={length} price={price} />
